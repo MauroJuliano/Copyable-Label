4391f04374db4501c124281f450e0c420ae0416a
MauroJuliano <43898163+MauroJuliano@users.noreply.github.com>
2021-07-15 18:27:55 -0400

    Initial Commit

---
+++ CopyableLabel/CopyableLabel.h
@@ -0,0 +1,18 @@
+//
+//  CopyableLabel.h
+//  CopyableLabel
+//
+//  Created by user195713 on 7/15/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CopyableLabel.
+FOUNDATION_EXPORT double CopyableLabelVersionNumber;
+
+//! Project version string for CopyableLabel.
+FOUNDATION_EXPORT const unsigned char CopyableLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CopyableLabel/PublicHeader.h>
+
+
